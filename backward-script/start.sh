625eb9f2bbee1cea9d1ed48fefa6e4f75b133e7d
Erik Walinda <81198401+yurayura-nmr@users.noreply.github.com>
2023-12-05 09:29:21 +0900

    Create start.sh

---
+++ backward-script/start.sh
@@ -0,0 +1,8 @@
+# For example, if the CG structure to be back-calculated is called "1p30_nm_cyclic-CG_to_backcalc.gro" 
+
+mkdir -p final_results
+
+./aru_initram.sh -f 1p30_nm_cyclic-CG_to_backcalc.gro -o aa_charmm.gro -to charmm36 -p ./topol.top 
+mv aa_charmm.gro final_results/1p30_nm_backcalculated.gro
+
+# can add more back calculation runs here
